5c4cef7fbb94b8fdd58c26b86a7d986f4fdbb836
Anil Tamang <anilbal7676@gmail.com>
2026-09-22 13:12:02 +0545

    fix links

--- components/layout/navbar/Navbar.tsx
+++ components/layout/navbar/Navbar.tsx
@@ -18,28 +18,28 @@ export default function Navbar() {
 
       <div className="flex items-center gap-4 text-slate-400">
         <Link
-          href="#"
+          href="https://www.facebook.com/yatraway.nepal.travels"
+          className="hover:text-blue-400 transition-colors p-2 bg-slate-800 rounded-full border border-slate-700"
+        >
+          <FaFacebook className="w-5 h-5" />
+        </Link>
+        <Link
+          href="https://www.instagram.com/yatraway_nepal_travel?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
           className="hover:text-blue-400 transition-colors p-2 bg-slate-800 rounded-full border border-slate-700"
         >
           <FaInstagram className="w-5 h-5" />
         </Link>
         <Link
-          href="#"
+          href="https://tiktok.com/@yatrawaynepaltravelstours"
           className="hover:text-blue-400 transition-colors p-2 bg-slate-800 rounded-full border border-slate-700"
         >
           <FaTiktok className="w-5 h-5" />
         </Link>
         <Link
-          href="#"
+          href="https://youtube.com/@yatrawaynepal"
           className="hover:text-blue-400 transition-colors p-2 bg-slate-800 rounded-full border border-slate-700"
         >
           <FaYoutube className="w-5 h-5" />
-        </Link>
-        <Link
-          href="#"
-          className="hover:text-blue-400 transition-colors p-2 bg-slate-800 rounded-full border border-slate-700"
-        >
-          <FaFacebook className="w-5 h-5" />
         </Link>
       </div>
     </nav>
